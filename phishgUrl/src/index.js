b71cf4ff248a90a4f57d49cb1481510c1820b2dc
bajes <a199036@siswa.ukm.edu.my>
2025-12-22 14:58:46 +0800

    Update index.js

--- phishgUrl/src/index.js
+++ phishgUrl/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';// This imports your App component above
-import './index.css';    // If you have a global CSS file
+import App from './App'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-
 );
